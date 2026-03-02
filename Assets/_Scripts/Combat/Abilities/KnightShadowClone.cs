1e1a2f095a0aebe9be9cbec0c856b766f216a05f
lki40 <blazichaos14@gmail.com>
2026-03-03 10:04:26 +1300

    Added shader for Knight clones

--- Assets/_Scripts/Combat/Abilities/KnightShadowClone.cs
+++ Assets/_Scripts/Combat/Abilities/KnightShadowClone.cs
@@ -33,8 +33,8 @@ public class KnightShadowClone : MonoBehaviour
         transform.position = startPosition;
 
         SpriteRenderer.sprite = sprite;
-        SpriteRenderer.material = material;
-        SpriteRenderer.color = tint;
+        //SpriteRenderer.material = material;
+        //SpriteRenderer.color = tint;
 
         if (takeoffVfx != null)
         {
